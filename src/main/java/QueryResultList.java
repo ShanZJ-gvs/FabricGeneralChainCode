82673bee0304a616cad1bc91af4fa06e72d76dc9
shanzj <gvssimux@qq.com>
2022-04-06 20:02:33 +0800

    测试成功，并且上传了演示视频到b站

---
+++ src/main/java/QueryResultList.java
@@ -0,0 +1,13 @@
+import lombok.Data;
+import org.hyperledger.fabric.contract.annotation.DataType;
+import org.hyperledger.fabric.contract.annotation.Property;
+
+import java.util.List;
+
+@DataType
+@Data
+public class QueryResultList {
+
+    @Property
+    List<QueryResult> resultList;
+}
